7a21ff98244fe348998e7bc8f5b3d455a66f56de
autom4ttic <autom4ttic@protonmail.com>
2021-11-20 14:48:46 -0300

    initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# Let's Go Brandon Site
+
+## Setup
+
+Install Node 16 LTS
+
+```sh
+sudo apt update
+sudo apt upgrade -y
+
+curl -fsSL https://deb.nodesource.com/setup_16.x | sudo -E bash -
+sudo apt install -y nodejs
+```
+
+## Dependencies
+
+
+## Running
+
+```
+npm run dev
+```
---
+++ pages/index.js
@@ -0,0 +1,210 @@
+import Head from 'next/head'
+import Link from 'next/link'
+import Image from 'next/image'
+
+const YourComponent = () => (
+  <Image
+    src="/images/profile.jpg" // Route of the image file
+    height={144} // Desired size with correct aspect ratio
+    width={144} // Desired size with correct aspect ratio
+    alt="Your Name"
+  />
+)
+
+export default function Home() {
+  return (
+    <div className="container">
+      <Head>
+        <title>Let's Go Brandon NFT</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        <h1 className="title">
+          Let's Go Brandon NFTs
+        </h1>
+
+        <p className="description">
+          A fun NFT project created to protect our freedoms
+        </p>
+
+        <Image
+          src="/images/logo.png"
+          height={250}
+          width={250}
+        />
+
+        <p className="description">
+          We have just started<br></br>
+          Join the community and contribute to our project:
+        </p>
+
+        <div style={{ display: 'inline-block' }}>
+          <div style={{ padding: 10, display: 'inline-block' }}>
+            <Link href="#">
+              <a><Image
+                src="/images/twitter.png"
+                height={50}
+                width={50}
+              /></a>
+            </Link>
+          </div>
+
+          <div style={{ padding: 10, display: 'inline-block' }}>
+            <Link href="https://discord.gg/S6F4cEurED">
+              <a><Image
+                src="/images/discord.png"
+                height={50}
+                width={50}
+              /></a>
+            </Link>
+          </div>
+        </div>
+
+      </main>
+
+      <style jsx>{`
+        .container {
+          min-height: 100vh;
+          padding: 0 0.5rem;
+          display: flex;
+          flex-direction: column;
+          justify-content: center;
+          align-items: center;
+        }
+
+        main {
+          padding: 5rem 0;
+          flex: 1;
+          display: flex;
+          flex-direction: column;
+          justify-content: center;
+          align-items: center;
+        }
+
+        footer {
+          width: 100%;
+          height: 100px;
+          border-top: 1px solid #eaeaea;
+          display: flex;
+          justify-content: center;
+          align-items: center;
+        }
+
+        footer img {
+          margin-left: 0.5rem;
+        }
+
+        footer a {
+          display: flex;
+          justify-content: center;
+          align-items: center;
+        }
+
+        a {
+          color: inherit;
+          text-decoration: none;
+        }
+
+        .title a {
+          color: #0070f3;
+          text-decoration: none;
+        }
+
+        .title a:hover,
+        .title a:focus,
+        .title a:active {
+          text-decoration: underline;
+        }
+
+        .title {
+          margin: 0;
+          line-height: 1.15;
+          font-size: 4rem;
+        }
+
+        .title,
+        .description {
+          text-align: center;
+        }
+
+        .description {
+          line-height: 1.5;
+          font-size: 1.5rem;
+        }
+
+        code {
+          background: #fafafa;
+          border-radius: 5px;
+          padding: 0.75rem;
+          font-size: 1.1rem;
+          font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
+            DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
+        }
+
+        .grid {
+          display: flex;
+          align-items: center;
+          justify-content: center;
+          flex-wrap: wrap;
+
+          max-width: 800px;
+          margin-top: 3rem;
+        }
+
+        .card {
+          margin: 1rem;
+          flex-basis: 45%;
+          padding: 1.5rem;
+          text-align: left;
+          color: inherit;
+          text-decoration: none;
+          border: 1px solid #eaeaea;
+          border-radius: 10px;
+          transition: color 0.15s ease, border-color 0.15s ease;
+        }
+
+        .card:hover,
+        .card:focus,
+        .card:active {
+          color: #0070f3;
+          border-color: #0070f3;
+        }
+
+        .card h3 {
+          margin: 0 0 1rem 0;
+          font-size: 1.5rem;
+        }
+
+        .card p {
+          margin: 0;
+          font-size: 1.25rem;
+          line-height: 1.5;
+        }
+
+        .logo {
+          height: 1em;
+        }
+
+        @media (max-width: 600px) {
+          .grid {
+            width: 100%;
+            flex-direction: column;
+          }
+        }
+      `}</style>
+
+      <style jsx global>{`
+        html,
+        body {
+          padding: 0;
+          margin: 0;
+          font-family: Comic Sans MS;
+        }
+        * {
+          box-sizing: border-box;
+        }
+      `}</style>
+    </div>
+  )
+}
