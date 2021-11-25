b9dc8b85abc1b307fc03a4e0bc42877ce0e3ea20
autom4ttic <autom4ttic@protonmail.com>
2021-11-25 07:14:50 -0300

    trump v1

--- pages/index.js
+++ pages/index.js
@@ -1,49 +1,67 @@
+import React, { useEffect } from 'react';
 import Head from 'next/head'
 import Layout, { siteTitle } from '../components/layout'
 import styles from './index.module.css'
 import utilStyles from '../styles/utils.module.css'
 import Image from 'next/image'
 import SocialLink from '../components/socialLink'
+import Trump from '../components/trump'
 
-export default function Home() {
-  return (
-    <Layout>
-      <Head>
-        <title>{siteTitle}</title>
-      </Head>
+export default class Home extends React.Component {
 
-      <header className={styles.title}>
-        Let&apos;s Go Brandon NFTs
-      </header>
+  constructor(props) {
+    super(props);
+    // this.changeChild = React.createRef();
+  }
 
-      <p className={styles.description}>
-        A fun NFT project created to protect our freedoms
-      </p>
+  // handleClick = () => {
+  //   this.changeChild.current.goTrump()
+  // }
 
-      <Image
-        src="/images/logo.png"
-        height={250}
-        width={250}
-        className={utilStyles.roundedImage}
-        alt=""
-      />
+  render() {
+    return (
+      <Layout>
+        <Head>
+          <title>{siteTitle}</title>
+        </Head>
 
-      <p className={styles.description}>
-        We have just started<br></br>
-        Join the community and contribute to our project:
-      </p>
+        <header className={styles.title}>
+          Let&apos;s Go Brandon NFTs
+        </header>
 
-      <div>
-        <SocialLink
-          href="https://twitter.com/GoBrandonNFT"
-          imgSrc="/images/twitter.png"
+        <p className={styles.description}>
+          A fun NFT project created to protect our freedoms
+        </p>
+
+        <Image
+          src="/images/logo.png"
+          height={250}
+          width={250}
+          className={utilStyles.roundedImage}
+          alt=""
+        // onMouseEnter={this.handleClick}
         />
-        <SocialLink
-          href="https://discord.gg/S6F4cEurED"
-          imgSrc="/images/discord.png"
-        />
-      </div>
 
-    </Layout>
-  )
+        <p className={styles.description}>
+          We have just started<br></br>
+          Join the community and contribute to our project:
+        </p>
+
+        <div>
+          <SocialLink
+            href="https://twitter.com/GoBrandonNFT"
+            imgSrc="/images/twitter.png"
+          />
+          <SocialLink
+            href="https://discord.gg/S6F4cEurED"
+            imgSrc="/images/discord.png"
+          />
+        </div>
+
+        <Trump/>
+
+      </Layout>
+    )
+  }
+
 }
